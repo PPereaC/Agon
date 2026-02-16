92baa5d5686b81cf2f8078f34e7b713730749295
Pablo Perea Campos <pablopereac02@gmail.com>
2026-02-16 18:49:08 +0100

    feat(Navbar): reemplazado logo de Acme por icono de Gamepad y actualizado diseño del Navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,19 +4,7 @@ import { Input } from "@heroui/input";
 
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownSection, DropdownItem } from "@heroui/dropdown";
 import { Avatar, AvatarGroup, AvatarIcon } from "@heroui/avatar";
-
-export const AcmeLogo = () => {
-    return (
-        <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-            <path
-                clipRule="evenodd"
-                d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-                fill="currentColor"
-                fillRule="evenodd"
-            />
-        </svg>
-    );
-};
+import { Gamepad2 } from "lucide-react";
 
 export const SearchIcon = ({ size = 24, strokeWidth = 1.5, width, height, ...props }) => {
     return (
@@ -50,46 +38,44 @@ export const SearchIcon = ({ size = 24, strokeWidth = 1.5, width, height, ...pro
 
 export const NavbarApp = () => {
     return (
-        <Navbar isBordered>
-            <NavbarContent justify="start">
-                <NavbarBrand className="mr-4">
-                    <AcmeLogo />
-                    <p className="hidden sm:block font-bold text-inherit">ACME</p>
+        <Navbar isBordered className="w-full bg-surface-darkest border-b border-primary h-13">
+            <NavbarContent justify="start" className="flex-1 items-center">
+                <NavbarBrand className="ml-8 mr-6">
+                    <Gamepad2 size={31} className="mr-2 text-primary mt-1" />
+                    <p className="font-primary font-bold text-primary text-2xl">LEXIS</p>
                 </NavbarBrand>
-                <NavbarContent className="hidden sm:flex gap-3">
-                    <NavbarItem>
-                        <Link color="foreground" href="#">
-                            Features
-                        </Link>
-                    </NavbarItem>
-                    <NavbarItem isActive>
-                        <Link aria-current="page" color="secondary" href="#">
-                            Customers
-                        </Link>
-                    </NavbarItem>
-                    <NavbarItem>
-                        <Link color="foreground" href="#">
-                            Integrations
-                        </Link>
-                    </NavbarItem>
-                </NavbarContent>
             </NavbarContent>
 
-            <NavbarContent as="div" className="items-center" justify="end">
-                <Input
-                    classNames={{
-                        base: "max-w-full sm:max-w-[10rem] h-10",
-                        mainWrapper: "h-full",
-                        input: "text-small",
-                        inputWrapper:
-                            "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-                    }}
-                    placeholder="Type to search..."
-                    size="sm"
-                    startContent={<SearchIcon size={18} />}
-                    type="search"
-                />
-                <Dropdown placement="bottom-end">
+            <NavbarContent className="hidden md:flex flex-1 justify-center gap-6 text-text-title">
+                <NavbarItem>
+                    <Link className="text-text-title hover:text-primary" href="#">
+                        Trending
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link className="text-text-title hover:text-primary" aria-current="page" href="#">
+                        Novedades
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link className="text-text-title hover:text-primary" href="#">
+                        Proximamente
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link className="text-text-title hover:text-primary" href="#">
+                        Eternos
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link className="text-text-title hover:text-primary" href="#">
+                        Generos
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+
+            <NavbarContent as="div" justify="end" className="items-center justify-end flex-1 gap-3 pr-4">
+                <Dropdown placement="bottom-end" className="bg-surface-darkest">
                     <DropdownTrigger>
                         <Avatar
                             isBordered
@@ -102,17 +88,9 @@ export const NavbarApp = () => {
                         />
                     </DropdownTrigger>
                     <DropdownMenu aria-label="Profile Actions" variant="flat">
-                        <DropdownItem key="profile" className="h-14 gap-2">
-                            <p className="font-semibold">Signed in as</p>
-                            <p className="font-semibold">zoey@example.com</p>
-                        </DropdownItem>
-                        <DropdownItem key="settings">My Settings</DropdownItem>
-                        <DropdownItem key="team_settings">Team Settings</DropdownItem>
-                        <DropdownItem key="analytics">Analytics</DropdownItem>
-                        <DropdownItem key="system">System</DropdownItem>
-                        <DropdownItem key="configurations">Configurations</DropdownItem>
-                        <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-                        <DropdownItem key="logout" color="danger">
+                        <DropdownItem key="settings" className="text-white">Mi biblioteca</DropdownItem>
+                        <DropdownItem key="settings" className="text-white">Ajustes</DropdownItem>
+                        <DropdownItem key="logout" className="text-red-500">
                             Log Out
                         </DropdownItem>
                     </DropdownMenu>
