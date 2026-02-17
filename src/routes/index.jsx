cec1740a2833fae7f39771526ef2b156f217cfad
Pablo Perea Campos <pablopereac02@gmail.com>
2026-02-17 18:23:32 +0100

    feat(AppRoutes): añadida ruta para la página de detalles de un videojuego

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import MainLayout from '../layouts/MainLayout'
 import NewNTrendingPage from '../pages/NewNTrendingPage'
 import LastReleasesPage from '../pages/LastReleasesPage'
+import VideoGameDetailsPage from '../pages/VideoGameDetailsPage'
 
 import '../App.css'
 
@@ -10,6 +11,7 @@ function AppRoutes() {
     <Routes>
       <Route path="/" element={<MainLayout><NewNTrendingPage /></MainLayout>} />
       <Route path="/ultimos-lanzamientos" element={<MainLayout><LastReleasesPage /></MainLayout>} />
+      <Route path="/juego/:id" element={<MainLayout><VideoGameDetailsPage /></MainLayout>} />
     </Routes>
   )
 }
